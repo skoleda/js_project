fc6bec358dc7587bf315bb88a22db88e4488226a
Sergey <koleda.exceedteam@gmail.com>
2021-07-01 09:28:31 +0300

    first task

---
+++ first.js
@@ -0,0 +1,13 @@
+let stri='Я изучаю JS';
+
+console.log(stri.charAt(2));
+console.log(stri.charAt(0));
+console.log(stri.charAt(stri.length-1));
+
+console.log(stri.slice(2, 3));
+console.log(stri.slice(0,1));
+console.log(stri.slice(stri.length-1,stri.length));
+
+console.log(stri.substring(2, 3));
+console.log(stri.substring(0,1));
+console.log(stri.substring(stri.length-1,stri.length));
